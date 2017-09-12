57223d3e9403bc7249e8e00c36c924d5128aa7ab
chengteng <teng.chen@colourdata.com.cn>
2017-09-12 14:47:03 +0800

    final version of e-com predict

--- ScorePrediction_ModelGeneration.py
+++ ScorePrediction_ModelGeneration.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-此篇为电商评论分数预测模型的训练集部分，主要功能为：
-1、读取训练集
-2、生成并保存模型
-3、模型效果评估
+this <training module> is used to generate prediction model, it contains functions:
+1) read and transform data
+2) generate and save model
+3) model evaluation
+By default, wo read data from file xlsx, and sheet name is "train"
+the 1st column is "content", the 2nd column is "score"
 
-训练集默认为xlsx文件，其中sheet名为train。
-sheet中第一列为content列，是文本内容
-sheet中第二列为score列，为分数（需要整数形式）
+一、此篇为电商评论分数预测模型的训练集部分，主要功能为：
+    1) 读取训练集
+    2) 生成并保存模型
+    3) 模型效果评估
+二、训练集默认为xlsx文件，其中sheet名为train。
+    sheet中第一列为content列，是文本内容
+    sheet中第二列为score列，为分数（需要整数形式）
 """
-from sklearn.feature_selection import SelectKBest, SelectPercentile, VarianceThreshold, f_classif
+from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
-from sklearn.feature_extraction.text import CountVectorizer, HashingVectorizer, TfidfVectorizer, TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
@@ -33,11 +39,15 @@ PATH_DATA = PATH_MAIN+"data/"
 PATH_MODEL = PATH_MAIN+"model/"
 TRAIN_SHEET = "train"
 
-# 第一级进行2类的分类，输入第二级进行2类的分类
+# read training set
 f1 = xlrd.open_workbook(os.path.join(PATH_DATA, "train.xlsx"))
-col_score = 1  # 确定第几列是label，从0开始
+col_score = 1  # column of label
+trainfile = f1.sheet_by_name(TRAIN_SHEET)
+trainrows = trainfile.nrows
+traincols = trainfile.ncols
+index = 0
 
-# 初始化数据
+# initialization
 train_N_ID = []
 train_N = []
 train_N_label = []
@@ -45,13 +55,7 @@ train_P_ID = []
 train_P = []
 train_P_label = []
 
-# 读入训练集原始文本，并生成标记
-trainfile = f1.sheet_by_name(TRAIN_SHEET)
-trainrows = trainfile.nrows
-traincols = trainfile.ncols
-index = 0
-
-# 正面训练集过多，因此随机抽样，再与负面训练集合并为训练集
+# shuffle positive data to avoid unbalanced training set
 for j in range(1, trainrows):
     if trainfile.cell_value(j, col_score) == '5' or trainfile.cell_value(j, col_score) == '4':
         train_P_ID.append(index)
@@ -62,57 +66,44 @@ for j in range(1, trainrows):
         train_N.append(trainfile.cell_value(j, 0))
         train_N_label.append(trainfile.cell_value(j, col_score))
 
-dummy_length = 5 * len(train_N)  # 正面训练集数量最多为负面训练集的5倍,避免过度不均衡
-slice_id = random.sample(train_P, min(len(train_P), dummy_length))
+# positive data is at most 5 times of negative data, in order to avoid unbalanced data set
+max_length = 5 * len(train_N)
+slice_id = random.sample(train_P, min(len(train_P), max_length))
 train = copy.copy(train_N)
 train_label = copy.copy(train_N_label)
 for j in range(len(slice_id)):
     train.append(train_P[slice_id[j]])
     train_label.append(train_P_label[slice_id[j]])
 
-train_label = np.asarray(train_label)  # .astype('string')
+train_label = np.asarray(train_label)
 
-# 通过2-gram生成词袋模型，再通过chi2进行优先选择，确定最后的特征向量空间
-# vectorizer = CountVectorizer(analyzer='char_wb', ngram_range=(2, 2), max_df=0.8, binary=True)
-# 向量生成器，并导入卡方的选择的特征词作为词袋，生成的向量为0，1的binary形式
-# vectorizer = CountVectorizer(analyzer='char_wb',ngram_range=(2,2),max_df=0.8,binary=True,max_features = 5000)
-# vectorizer = CountVectorizer(analyzer='char_wb',ngram_range=(2,2),min_df=3,max_df=0.8,binary=True)
-# vectorizer = HashingVectorizer(analyzer='char_wb' ,non_negative = True,binary=True)
+# generate 2-gram model and select features by chi2，then generate final vector space
 vectorizer = TfidfVectorizer(analyzer='char_wb', ngram_range=(2, 2), max_df=0.8, binary=True)
-
-
 ChiVectorizer = SelectKBest(chi2, k=4000)
-# 卡方作为特征选择的算法
-# ChiVectorizer = SelectPercentile(f_classif, percentile=50)
-# ChiVectorizer=SelectKBest(f_classif, k=4000)
-# ChiVectorizer=SelectPercentile(chi2, percentile=30)
-# ChiVectorizer=SelectPercentile(percentile=40)
-# ChiVectorizer=VarianceThreshold()
-# ChiVectorizer=GenericUnivariateSelect( mode='percentile')
 
-# 将生成的特征空间方法运用到训练集上
-fea_train = vectorizer.fit_transform(train)  # 生成训练集0，1向量
-trainvec = ChiVectorizer.fit_transform(fea_train, train_label)  # 由卡方选择的特征生成训练集向量
-
+# apply on training set
+fea_train = vectorizer.fit_transform(train)
+trainvec = ChiVectorizer.fit_transform(fea_train, train_label)
 train_array = np.asarray(trainvec)
 train_label_array = np.asarray(train_label)
 
-print("卡方计算完毕！！！")
+print("chi2 finished")
 print(datetime.datetime.now())
 
-# 分别使用逻辑回归和随机森林进行判别，然后采用投票来产出最后结果。并显示出两个模型的score(实际两个模型效果差不多)
-lrclf = LogisticRegression(C=1e5)  # lr分类器
-lrclf.fit(trainvec, train_label)  # 根据训练集和标签生成模型
+# use multi-algorithm and vote for the final prediction
+# logistic regression
+lrclf = LogisticRegression(C=1e5)
+lrclf.fit(trainvec, train_label)
 scores = cross_val_score(lrclf, trainvec, train_label, cv=10).mean()
 print("lr train set score:"+str(scores))
 
-rfclf = RandomForestClassifier()  # 随机森林
+# random forest
+rfclf = RandomForestClassifier()
 rfclf.fit(trainvec, train_label)
 scores = cross_val_score(rfclf, trainvec, train_label, cv=10).mean()
 print("rf train set score:"+str(scores))
 
-
-# 模型存储下来，以便在测试集上使用
+# model storage
 joblib.dump(lrclf, os.path.join(PATH_MODEL, "lrclf.m"))
 joblib.dump(rfclf, os.path.join(PATH_MODEL, "rfclf.m"))
 joblib.dump(ChiVectorizer, os.path.join(PATH_MODEL, "ChiVectorizer.m"))
--- ScorePrediction_Predict.py
+++ ScorePrediction_Predict.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-此篇为电商评论分数预测模型的应用部分，主要功能为：
-1、读取需要预测的数据
-2、预测分数并存储
+this <predict module> is used to predict comments' scores, it contains functions:
+1) read and transform data
+2) predict score and vote for the result
+By default, we read data from file xlsx, and sheet name is "test"
+it contains only 1 column, is "content"
+the result file is format xls, and sheet name is "predict_result"
+the 1st column is "content",
+the 2nd column is "lr_label" and the 3rd column is "rf_label"
+and 4th column is the final result
 
-测试集默认为xlsx文件，其中sheet名为test。
-sheet中只有一列，为content列，是文本内容
-
-默认输出结果为xls文件，sheet名为predict_result
-第一列为content，是对应的文本内容
-第二列为lr_label，第三列为rf_label,分别是两种模型预测的分数
-第四列为label，是最终的打分。（具体的投票规则可改）
+一、此篇为电商评论分数预测模型的应用部分，主要功能为：
+    1) 读取需要预测的数据
+    2) 预测分数并存储
+二、测试集默认为xlsx文件，其中sheet名为test。
+    sheet中只有一列，为content列，是文本内容
+三、默认输出结果为xls文件，sheet名为predict_result
+    第一列为content，是对应的文本内容
+    第二列为lr_label，第三列为rf_label,分别是两种模型预测的分数
+    第四列为label，是最终的打分。（具体的投票规则可改）
 """
 import xlrd
 import xlwt
@@ -27,19 +35,17 @@ PATH_DATA = PATH_MAIN+"data/"
 PATH_MODEL = PATH_MAIN+"model/"
 TEST_SHEET = "test"
 
-lrclf = joblib.load(os.path.join(PATH_MODEL, "lrclf.m"))  # 保存分类器模型
-rfclf = joblib.load(os.path.join(PATH_MODEL, "rfclf.m"))  # 保存分类器模型
+lrclf = joblib.load(os.path.join(PATH_MODEL, "lrclf.m"))
+rfclf = joblib.load(os.path.join(PATH_MODEL, "rfclf.m"))
 ChiVectorizer = joblib.load(os.path.join(PATH_MODEL, "ChiVectorizer.m"))
-# 载入卡方选择后的训练集模型，根据该模型来转化测试集为相应的0，1向量
 vectorizer = joblib.load(os.path.join(PATH_MODEL, "vectorizer.m"))
-# 载入训练集模型，根据该模型来转化测试集为相应的0，1向量
 
-# 初始化数据
+# initialization
 predict = []
 predict_label = []
 score_list = []
 
-# 读入测试集原始文本
+# read test data
 test_file = xlrd.open_workbook(os.path.join(PATH_DATA, "predict.xlsx"))
 predictfile = test_file.sheet_by_name(TEST_SHEET)
 predictrows = predictfile.nrows
@@ -47,16 +53,16 @@ testcols = predictfile.ncols
 for j in range(1, predictrows):
     predict.append(predictfile.cell_value(j, 0))
 
-print("数据读取完毕")
+print("data read")
 print(datetime.datetime.now())
 
-# 进行模型判断
-fea_predict = vectorizer.transform(predict)  # 生成测试集0,1向量
-predict_vec = ChiVectorizer.transform(fea_predict)  # 由卡方选择的特征生成测试集向量
+# predict
+fea_predict = vectorizer.transform(predict)
+predict_vec = ChiVectorizer.transform(fea_predict)
 pred1 = lrclf.predict(predict_vec)
 pred2 = rfclf.predict(predict_vec)
 
-print("逻辑回归和随机森林计算完毕！！！")
+print("prediction finished")
 print(datetime.datetime.now())
 
 # prepare result file
@@ -74,8 +80,4 @@ for j in range(0, predict_len):
         sheet.write(j + 1, 1, pred1[j])
         sheet.write(j + 1, 2, pred2[j])
         sheet.write(j + 1, 3, str(min(int(pred1[j]), int(pred2[j]))))
-        # if int(pred1[j]) < 4 and int(pred2[j]) < 4:
-        #     sheet.write(j + 1, 3, str(min(int(pred1[j]), int(pred2[j]))))
-        # else:
-        #     sheet.write(j + 1, 3, str(max(int(pred1[j]), int(pred2[j]))))
 result_file.save(os.path.join(PATH_DATA, "predict.xls"))
